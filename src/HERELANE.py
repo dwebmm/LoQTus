5699aa8faeb0e08f9491045270ac08e0c192c288
Michael Maurakis <84732377+dwebmm@users.noreply.github.com>
2022-05-19 21:50:16 -0500

    Add files via upload

---
+++ src/HERELANE.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+
+# Use Qprocess in order to execute this command as a part of the process dolphin process
+
+# FULLNODE_API_INFO=wss://api.chain.love lotus daemon --lite
+
+
+
+# if __name__ == "__main__":
+#     pass
